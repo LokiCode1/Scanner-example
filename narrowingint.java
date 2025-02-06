61864bd37895ba9d2807550f55d578bc5855a2f4
LokiCode1 <rushikeshkengar648@gmail.com>
2025-02-06 23:38:49 +0530

    typecasting

---
+++ narrowingint.java
@@ -0,0 +1,19 @@
+class NarrowingInt
+
+{
+	public static void main(String[] args) 
+	{
+
+		
+		int d = 14141414;
+		short e = (short)d;
+		char f =(char)d;
+		byte g = (byte)d;
+		
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}   
+}
---
+++ narrowinglong - Copy.java
@@ -0,0 +1,20 @@
+class NarrowingLong 
+
+{
+	public static void main(String[] args) 
+	{
+
+		long c = 12121212l;
+		int d = (int)c;
+		short e = (short)c;
+		char f =(char)c;
+		byte g = (byte)c;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}   
+}
